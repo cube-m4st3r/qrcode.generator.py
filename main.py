9c9db9b096a65c09f6d8c57d8c7c0146e291b92d
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-04-20 23:18:19 +0200

    implemented logo input & better runtime

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-import pyqrcode, png
+# import modules
+import qrcode
+from PIL import Image
 
-qr = pyqrcode.create("link")
-qr.png('qrcode.png', scale=8)
+
+def run():
+
+    # taking image which user wants
+    # in the QR code center
+    Logo_link = 'logo.jpg'
+
+    logo = Image.open(Logo_link)
+
+    # taking base width
+    basewidth = 100
+
+    # adjust image size
+    wpercent = (basewidth/float(logo.size[0]))
+    hsize = int((float(logo.size[1])*float(wpercent)))
+    logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+    QRcode = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    url = ''
+
+    QRcode.add_data(url)
+
+    QRcode.make()
+
+    QRimg = QRcode.make_image(
+        fill_color="black", back_color="white").convert('RGB')
+
+    pos = ((QRimg.size[0] - logo.size[0]) // 2,
+        (QRimg.size[1] - logo.size[1]) // 2)
+    QRimg.paste(logo, pos)
+
+    QRimg.save('qrcode.png')
+
+    print('QR code generated!')
+
+if __name__ == "__main__":
+    run()
